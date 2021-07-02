ec3cda1b0f3f1f0dad61101be22d0ebac2480bd4
little-pinecone <marta.szymek.mail@gmail.com>
2021-07-02 11:31:26 +0200

    Update Docker installation script

--- scripts/tools/docker.sh
+++ scripts/tools/docker.sh
@@ -2,15 +2,16 @@ apt install \
   apt-transport-https \
   ca-certificates \
   curl \
-  gnupg-agent \
-  software-properties-common \
+  gnupg \
+  lsb-release \
   -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+curl curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update -y
 apt install docker-ce docker-ce-cli containerd.io -y
 apt install docker-compose
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
